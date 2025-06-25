aa6a071b1f90a3fc84c0d8c8e4a896c367cf0cee
tilltnet <github@tillt.net>
2025-06-24 21:15:08 -0400

    tests: formalized download_feeds() test

--- tests/testthat/test-download_feeds.R
+++ tests/testthat/test-download_feeds.R
@@ -1,10 +1,12 @@
 test_that("download_feeds() works", {
   gtfs_de <-
     get_gtfs_feeds(country_code = "DE", offset = 10) |>
-    dplyr::slice(1:3)
+    dplyr::slice(1)
 
   tmp_download_path <- tempdir()
 
-  download_feeds(gtfs_de, tmp_download_path)
+  expect_no_error(
+    download_feeds(gtfs_de, tmp_download_path)
+  )
   #browseURL(tmp_download_path)
 })
